2e7b2ffd9271eea9c70b2f90940d6f12f5ad8fa6
Maureen <maureenmontano149@gmail.com>
2022-06-07 13:22:21 -0600

    Curso de JS

---
+++ cursojs.js
@@ -0,0 +1,20 @@
+console.log("Hola mundo");
+
+//let c;
+//let f = 90
+//c = (90 - 32) * (5/9);
+//console.log(c);
+
+let nombre = "Maureen";
+let apellido = "Montano";
+
+let nombreCompleto = nombre + " " + apellido;
+console.log(nombreCompleto);
+
+let a = 20;
+let b = 10;
+console.log(a+b);
+
+let activo = true;
+let despedido = false;
+console.log();
